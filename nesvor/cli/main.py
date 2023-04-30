8fc99e74cc21cbc6ac0dde3543ae567fb20a2820
daviddmc <junshen@mit.edu>
2023-04-30 17:16:32 -0400

    update parser help info

--- nesvor/cli/main.py
+++ nesvor/cli/main.py
@@ -1,12 +1,16 @@
 import argparse
 import sys
 from typing import Union
-from .commands import *
-from ..utils import setup_logger
 import torch
 import random
 import numpy as np
 import string
+from .commands import *
+from ..utils import setup_logger
+from .. import __version__
+
+
+# formatter classes
 
 
 class Formatter(
@@ -21,8 +25,40 @@ class FormatterMetavar(Formatter, argparse.MetavarTypeHelpFormatter):
     pass
 
 
-def update_defaults(parser: argparse.ArgumentParser, **kwargs):
-    parser.set_defaults(**kwargs)
+class SubcommandHelpFormatter(Formatter):
+    def _format_action(self, action):
+        parts = super()._format_action(action)
+        L = self._max_help_position - 2
+        if action.nargs == 0:
+            parts_list = parts.split()
+            i = 0
+            while parts_list[i][0] == "-":
+                i += 1
+            parts1 = " ".join(parts_list[:i])
+            parts2 = " ".join(parts_list[i:])
+            parts = "  " + parts1 + " " * (L - len(parts1)) + parts2 + "\n"
+        if action.nargs == argparse.PARSER:
+            parts_list = parts.split("\n")[1:]
+            parts_list_combined = []
+            i = 0
+            while i < len(parts_list):
+                if not parts_list[i]:
+                    i += 1
+                elif len(parts_list[i].split()) > 1:
+                    parts_list_combined.append(parts_list[i])
+                    i += 1
+                else:
+                    parts_list_combined.append(parts_list[i] + " " + parts_list[i + 1])
+                    i += 2
+            for i in range(len(parts_list_combined)):
+                parts_list_list = parts_list_combined[i].split()
+                parts1 = parts_list_list[0]
+                parts2 = " ".join(parts_list_list[1:])
+                parts_list_combined[i] = (
+                    "  " + parts1 + " " * (L - len(parts1)) + parts2
+                )
+            parts = "\n".join(parts_list_combined) + "\n"
+        return parts
 
 
 # parents parsers
@@ -675,6 +711,10 @@ def build_parser_common() -> argparse.ArgumentParser:
     return _parser
 
 
+def update_defaults(parser: argparse.ArgumentParser, **kwargs):
+    parser.set_defaults(**kwargs)
+
+
 # command parsers
 
 
@@ -897,15 +937,18 @@ def build_command_segment_volume(
 def main() -> None:
     parser = argparse.ArgumentParser(
         prog="nesvor",
-        description="NeSVoR: a toolkit for neural slice-to-volume reconstruction",
+        description=f"NeSVoR: a toolkit for neural slice-to-volume reconstruction (v{__version__})",
         epilog="Run 'nesvor COMMAND --help' for more information on a command.\n\n"
         + "To learn more about NeSVoR, check out our repo at "
         + "https://github.com/daviddmc/NeSVoR",
-        formatter_class=Formatter,
-        add_help=False,
+        formatter_class=SubcommandHelpFormatter,
     )
-    parser.add_argument("-h", "--help", action="help", help=argparse.SUPPRESS)
-    subparsers = parser.add_subparsers(title="commands", metavar=None, dest="command")
+    parser.add_argument(
+        "-v", "--version", action="version", version="%(prog)s v" + __version__
+    )
+    subparsers = parser.add_subparsers(
+        title="commands", metavar="COMMAND", dest="command"
+    )
 
     parser_reconstruct = build_command_reconstruct(subparsers)
     parser_sample_volume = build_command_sample_volume(subparsers)
