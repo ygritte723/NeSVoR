c84c10420880e31e0d312b891b40ad37d373da8d
daviddmc <junshen@mit.edu>
2023-05-13 22:49:41 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -43,10 +43,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
 
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# if not on_rtd:  # only import and set the theme if we're building docs locally
+#    import sphinx_rtd_theme
+#
+#    html_theme = "sphinx_rtd_theme"
+#    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ["_static"]
